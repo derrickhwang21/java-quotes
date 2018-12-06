0449f279812def47b9e54314b12ec21f146092cf
dhwang21 <hwang.derrick@gmail.com>
2018-12-06 12:43:59 -0800

    repushing

---
+++ src/main/java/Authors.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Authors {
+    public String[] tags;
+    public String author;
+    public String likes;
+    public String text;
+//    public ArrayList<String> listProperties;
+
+
+    public Authors(String[] tags, String name, String likes, String quotes){
+        this.tags = tags;
+        this.author = name;
+        this.likes = likes;
+        this.text = quotes;
+//        this.listProperties = listProperties;
+
+    }
+
+    public String toString(){
+       return this.author + this.likes + this.text;
+    }
+
+
+}
